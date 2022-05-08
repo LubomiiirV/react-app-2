cf4eb5dfe3e266ab23e48b4ef67c0b387d4ccfc3
Lubomir Vaclavik <vaclaviklubomir@gmail.com>
2022-05-08 16:54:32 +0200

    removed div from layout jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,13 +1,13 @@
 function Layout() {
   return (
-    <div>
+    <>
       <header>
         <h1>My First SPA</h1>
       </header>
       <main>
         <p>Tu bude obsah stránky</p>
       </main>
-    </div>
+    </>
   );
 }
 
