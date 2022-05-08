3cfaa1628e8c54325d452d186ff20ba7c350a5a6
Lubomir Vaclavik <vaclaviklubomir@gmail.com>
2022-05-08 16:52:33 +0200

    pokracovanie v tvorbe kostry v router branchi

---
+++ src/Layout.jsx
@@ -0,0 +1,14 @@
+function Layout() {
+  return (
+    <div>
+      <header>
+        <h1>My First SPA</h1>
+      </header>
+      <main>
+        <p>Tu bude obsah stránky</p>
+      </main>
+    </div>
+  );
+}
+
+export default Layout;
