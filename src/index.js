8f0c6861965c1f83545bbbbf85315920365a7810
Lubomir Vaclavik <vaclaviklubomir@gmail.com>
2022-05-08 16:55:04 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Layout from "./Layout";
 
-const container = document.querySelector("app-container");
+const container = document.querySelector("app-layout");
 const root = ReactDOM.createRoot(container);
 
 root.render(
