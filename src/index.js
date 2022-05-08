d1759e3b02f8d12a42da35c186b7e12c8e0110a3
Lubomir Vaclavik <vaclaviklubomir@gmail.com>
2022-05-08 18:02:55 +0200

    Revert "merge router to dev"
    This reverts commit 956879cae61cbf6234cf323844e2fe3d7b19cbe8.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./Views/Home";
@@ -8,11 +7,6 @@ import About from "./Views/About";
 import Page404 from "./Views/Page404";
 
 const container = document.querySelector("app-layout");
-=======
-import App from "./App";
-
-const container = document.getElementById("root");
->>>>>>> dev
 const root = ReactDOM.createRoot(container);
 
 root.render(
