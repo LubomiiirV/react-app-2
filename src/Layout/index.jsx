4449b6f3440d96821aa9cc5aa4aed491d24787e2
Lubomir Vaclavik <vaclaviklubomir@gmail.com>
2022-05-08 17:15:58 +0200

    Added routes/route

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -1,12 +1,18 @@
 import "./index.css";
+import { Link, Outlet } from "react-router-dom";
+
 function Layout() {
   return (
     <>
       <header>
         <h1>My First SPA</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
       </header>
       <main>
-        <p>Tu bude obsah stránky</p>
+        <Outlet />
       </main>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
+import Home from "./Views/Home";
+import About from "./Views/About";
 
 const container = document.querySelector("app-layout");
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Layout />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="" element={<Home />}></Route>
+
+        <Route path="about" element={<About />}></Route>
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
