57630ea4ac89e34398e9ff7cc29db702694c1a85
Lubomir Vaclavik <vaclaviklubomir@gmail.com>
2022-05-09 13:20:00 +0200

    added Footer

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Layout from "./Layout";
 import Home from "./Views/Home";
 import About from "./Views/About";
 import Page404 from "./Views/Page404";
+import Footer from "./Views/Footer";
 
 const container = document.querySelector("app-layout");
 const root = ReactDOM.createRoot(container);
@@ -16,6 +17,7 @@ root.render(
         <Route path="" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="*" element={<Page404 />} />
+        <Route path="footer" element={<Footer />} />
       </Route>
     </Routes>
   </BrowserRouter>
